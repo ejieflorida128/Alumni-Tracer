0de7c2e2be8c1f0a0864f61b8850660e03ad87a5
ejieflorida128 <ejieflorida128@gmail.com>
2024-10-16 13:57:35 +0800

    [ejie]
    updating database , importing r_accounts and deleting already imported table in sql_tables

--- database/alumni_tracer.sql
+++ database/alumni_tracer.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 15, 2024 at 12:30 PM
+-- Generation Time: Oct 16, 2024 at 07:56 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -30,8 +30,41 @@ SET time_zone = "+00:00";
 CREATE TABLE `e_schools` (
   `id` int(11) NOT NULL,
   `school_name` varchar(255) NOT NULL,
-  `logo` varchar(255) NOT NULL
+  `logo` varchar(255) NOT NULL,
+  `confirm_status` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `e_schools`
+--
+
+INSERT INTO `e_schools` (`id`, `school_name`, `logo`, `confirm_status`) VALUES
+(3, 'Cebu Eastern College', 'logo/cec.png', 'Approved'),
+(8, 'Southern Leyte State University', 'logo/slsu_to.png', 'Approved'),
+(9, 'University of the Philippines', 'logo/up.png', 'Pending');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `r_accounts`
+--
+
+CREATE TABLE `r_accounts` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `school_role` varchar(100) NOT NULL,
+  `school` varchar(255) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `date_registered` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `r_accounts`
+--
+
+INSERT INTO `r_accounts` (`id`, `name`, `email`, `school_role`, `school`, `password`, `date_registered`) VALUES
+(1, 'ralph miole', 'ralph@gmail.com', 'Principal', 'Sogod National Highschool', '$2y$10$Zz6xJc42yto8QU7X6lEnxO014goWxKFfHu6Yy/si8sRjMTWzE/qPO', '2024-10-16 09:51:43');
 
 --
 -- Indexes for dumped tables
@@ -44,6 +77,12 @@ ALTER TABLE `e_schools`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `r_accounts`
+--
+ALTER TABLE `r_accounts`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -51,7 +90,13 @@ ALTER TABLE `e_schools`
 -- AUTO_INCREMENT for table `e_schools`
 --
 ALTER TABLE `e_schools`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `r_accounts`
+--
+ALTER TABLE `r_accounts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
