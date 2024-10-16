b7c4904f0fddeb22fc7ccb380ed9690e7b799a23
Ralph A. Miole <150051239+miole2001@users.noreply.github.com>
2024-10-16 13:46:45 +0800

    Update register.php

--- php_files/register.php
+++ php_files/register.php
@@ -4,11 +4,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 // Database connection
-$connection = mysqli_connect("localhost", "root", "", "alumni");
-
-if ($connection->connect_error) {
-    die("Connection failed: " . $connection->connect_error);
-}
+include("conn.php");
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Sanitize user input
