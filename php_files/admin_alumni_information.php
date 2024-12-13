90c1b1725a4189ad4b33f494b7bf4440cc4b0a43
ejieflorida128 <ejieflorida128@gmail.com>
2024-12-13 09:58:39 +0800

    [ejie]
    changes from other files

--- php_files/admin_alumni_information.php
+++ php_files/admin_alumni_information.php
@@ -2,6 +2,26 @@
 session_start();
 include("../connection/conn.php");
 
+define('ENCRYPTION_KEY', getenv('MY_SECRET_KEY'));
+function decryptData($encryptedData)
+{
+    $key = ENCRYPTION_KEY;
+    $data = base64_decode($encryptedData);
+    $ivLength = openssl_cipher_iv_length('aes-256-cbc');
+    $iv = substr($data, 0, $ivLength); // Extract the IV
+    $encrypted = substr($data, $ivLength); // Extract the encrypted data
+    return openssl_decrypt($encrypted, 'aes-256-cbc', $key, 0, $iv);
+}
+
+
+function encryptData($data)
+{
+    $key = ENCRYPTION_KEY;
+    $iv = random_bytes(openssl_cipher_iv_length('aes-256-cbc')); // Generate a random IV
+    $encrypted = openssl_encrypt($data, 'aes-256-cbc', $key, 0, $iv);
+    return base64_encode($iv . $encrypted); // Store IV and encrypted data together
+}
+
 // Handle the deletion logic
 if (isset($_GET['delete_id'])) {
     $delete_id = $_GET['delete_id'];
@@ -29,6 +49,12 @@ if (isset($_POST['update_id'])) {
 
 // Fetch all records to display
 $select_info = mysqli_query($conn, "SELECT * FROM l_study_response");
+
+$id = $_SESSION['user_id'];
+$sql = "SELECT * FROM r_accounts WHERE id = '$id'";
+$query = mysqli_query($conn, $sql);
+$call = mysqli_fetch_assoc($query);
+
 
 ?>
 
@@ -83,11 +109,11 @@ $select_info = mysqli_query($conn, "SELECT * FROM l_study_response");
                 </a>
                 <div class="d-flex align-items-center ms-4 mb-4">
                     <div class="position-relative">
-                        <img class="rounded-circle" src="../template/img/user.jpg" alt="" style="width: 40px; height: 40px;">
+                        <img class="rounded-circle" src="<?php echo $call['profile_img'];  ?>" alt="" style="width: 40px; height: 40px;">
                         <div class="bg-success rounded-circle border border-2 border-white position-absolute end-0 bottom-0 p-1"></div>
                     </div>
                     <div class="ms-3">
-                        <h6 class="mb-0"><?php echo $call["name"] ?? 'N/A'; ?></h6>
+                        <h6 class="mb-0"><?php echo decryptData($call["name"]) ?? 'N/A'; ?></h6>
                         <span>Admin</span>
                     </div>
                 </div>
@@ -152,8 +178,8 @@ $select_info = mysqli_query($conn, "SELECT * FROM l_study_response");
                     </div>
                     <div class="nav-item dropdown">
                         <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
-                            <img class="rounded-circle me-lg-2" src="../template/img/user.jpg" alt="" style="width: 40px; height: 40px;">
-                            <span class="d-none d-lg-inline-flex">John Doe</span>
+                            <img class="rounded-circle me-lg-2" src="<?php echo $call['profile_img'];  ?>" alt="" style="width: 40px; height: 40px;">
+                            <span class="d-none d-lg-inline-flex"><?php echo decryptData($call["name"]) ?? 'N/A'; ?></span>
                         </a>
                         <div class="dropdown-menu dropdown-menu-end bg-light border-0 rounded-0 rounded-bottom m-0">
                             <a href="admin_profile.php" class="dropdown-item">My Profile</a>
--- php_files/admin_profile.php
+++ php_files/admin_profile.php
@@ -147,7 +147,7 @@ $call = mysqli_fetch_assoc($query);
                 </a>
                 <div class="d-flex align-items-center ms-4 mb-4">
                     <div class="position-relative">
-                        <img class="rounded-circle" src="../template/img/user.jpg" alt="" style="width: 40px; height: 40px;">
+                        <img class="rounded-circle" src="<?php echo $call['profile_img'];  ?>" alt="" style="width: 40px; height: 40px;">
                         <div class="bg-success rounded-circle border border-2 border-white position-absolute end-0 bottom-0 p-1"></div>
                     </div>
                     <div class="ms-3">
@@ -218,7 +218,7 @@ $call = mysqli_fetch_assoc($query);
                     </div>
                     <div class="nav-item dropdown">
                         <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
-                            <img class="rounded-circle me-lg-2" src="../template/img/user.jpg" alt="" style="width: 40px; height: 40px;">
+                            <img class="rounded-circle me-lg-2" src="<?php echo $call['profile_img'];  ?>" alt="" style="width: 40px; height: 40px;">
                             <span class="d-none d-lg-inline-flex"><?php echo decryptData($call["name"] ) ?? 'N/A'; ?></span>
                         </a>
                         <div class="dropdown-menu dropdown-menu-end bg-light border-0 rounded-0 rounded-bottom m-0">
